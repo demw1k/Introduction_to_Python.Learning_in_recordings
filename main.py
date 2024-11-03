d9f97cb9242492be6b4d818e03a4d538385c817e
demw1k <Zotov.yura1991@yandex.ru>
2024-11-03 23:52:15 +0400

    add main fail for lection_1, readme, gitignore

---
+++ main.py
@@ -0,0 +1,57 @@
+# n = 'No\"ne'
+# print(n)
+
+# # n = "sdfs"
+# #print(type(n))
+# n = 'safasdf'
+
+# a = 5
+# b= 5.89
+# c= 'hello'
+# #print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+# print("Введите первую строку")
+# a = input()
+# b = input("Введите второе число: ")
+# print(a, '+' ,b, '=', a+b)
+# #print(a)
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# v = 'sgdsadg'
+# v = int(v)
+
+# print("Введите первую строку")
+# a = int(input())
+# b = int(input("Введите второе число: "))
+# print(a, '+' , b , '=', a + b)
+
+# a = 5.5675
+# b = 5.67686
+# print(round(a*b,3))
+
+# a = 'qwe'
+# b = 'qwe'
+# c = a == b
+# print(c)
+
+# r = range(100,21,-20)
+
+# for i in r:
+#     print(i)
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"  
+#     print(line)
